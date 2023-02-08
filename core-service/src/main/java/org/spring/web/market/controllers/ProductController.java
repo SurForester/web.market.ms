bb1f4f1ad861d2c477ccfcf0a2169f6145fa7a0e
SurForester <92782670+SurForester@users.noreply.github.com>
2023-02-08 07:34:21 +0300

    Cache pattern.

--- core-service/src/main/java/org/spring/web/market/controllers/ProductController.java
+++ core-service/src/main/java/org/spring/web/market/controllers/ProductController.java
@@ -8,10 +8,10 @@ import org.spring.web.market.services.ImageSaverService;
 import org.spring.web.market.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/products")
@@ -21,19 +21,27 @@ public class ProductController {
     private CategoryService categoryService;
     private ImageSaverService imageSaverService;
     private ProductDTOConverter productDTOConverter;
+    private Map<Long, Product> productCache;
+
+    public ProductController() {
+        this.productCache = new HashMap<>();
+    }
 
     @Autowired
     public void setProductService(ProductService productService) {
         this.productService = productService;
     }
+
     @Autowired
     public void setCategoryService(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
+
     @Autowired
     public void setImageSaverService(ImageSaverService imageSaverService) {
         this.imageSaverService = imageSaverService;
     }
+
     @Autowired
     public void setProductDTOConverter(ProductDTOConverter productDTOConverter) {
         this.productDTOConverter = productDTOConverter;
@@ -41,7 +49,18 @@ public class ProductController {
 
     @GetMapping("/{id}")
     public ProductDTO getProductDTO(@PathVariable("id") Long id) {
-        Product product = productService.getProductById(id);
-        return productDTOConverter.modelProductItemToDTO(product);
+        return productDTOConverter.modelProductItemToDTO(getProduct(id));
     }
+
+    private Product getProduct(Long id) {
+        Product result;
+        if (productCache.containsKey(id)) {
+            result = productCache.get(id);
+        } else {
+            result = productService.getProductById(id);
+            productCache.put(id, result);
+        }
+        return result;
+    }
+
 }
